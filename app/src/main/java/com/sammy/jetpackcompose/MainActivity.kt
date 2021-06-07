cf9f3352d2c027a4f09f914bb01a4e7424567887
Sammy Mutahi <sammymutahi12@gmail.com>
2021-06-07 12:25:06 +0300

    Add ImageResource

--- app/src/main/java/com/sammy/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/sammy/jetpackcompose/MainActivity.kt
@@ -3,9 +3,16 @@ package com.sammy.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,14 +23,30 @@ class MainActivity : ComponentActivity() {
     }
     @Composable
     fun NewsStroy(){
-        Text(text = "A day in shark fin cove")
-        Text(text = "Davenport, California")
-        Text(text = "December 2018")
+        Column(
+            modifier = Modifier.padding(all = 16.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.header),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(180.dp)
+                    .fillMaxWidth(),
+                contentScale = ContentScale.Crop
+
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(text = "A day in shark fin cove")
+            Text(text = "Davenport, California")
+            Text(text = "December 2018")
+        }
+
     }
 
     @Preview
     @Composable
-    fun PreviewNewStory(){
+    fun DefaultPreview(){
         NewsStroy()
     }
 
