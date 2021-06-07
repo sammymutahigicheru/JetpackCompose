a9741101fc4957042ad17829097e5bc5ec44f0df
Sammy Mutahi <sammymutahi12@gmail.com>
2021-06-07 15:56:14 +0300

    Enable Reusability

--- app/src/main/java/com/sammy/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/sammy/jetpackcompose/MainActivity.kt
@@ -3,20 +3,18 @@ package com.sammy.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.material.MaterialTheme.typography
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sammy.jetpackcompose.ui.theme.JetpackComposeTheme
@@ -26,7 +24,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                NewsStroy()
+                NewNames()
             }
         }
     }
@@ -41,43 +39,12 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun NewsStroy() {
+    fun NewNames(names: List<String> = List(100) { "Hello $it" }) {
         val counterState = remember { mutableStateOf(0) }
         Column(
             modifier = Modifier.fillMaxHeight()
         ) {
-            Column(modifier = Modifier.weight(1f)) {
-                Image(
-                    painter = painterResource(id = R.drawable.header),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .height(180.dp)
-                        .fillMaxWidth()
-                        .clip(shape = RoundedCornerShape(4.dp)),
-                    contentScale = ContentScale.Crop
-
-                )
-                Spacer(modifier = Modifier.height(16.dp))
-                Text(
-                    text = "A day wandering through the sandhills " +
-                            "in Shark Fin Cove, and a few of the " +
-                            "sights I saw",
-                    style = typography.h6,
-                    maxLines = 2,
-                    overflow = TextOverflow.Ellipsis
-                )
-                Divider(color = Color.LightGray)
-                Text(
-                    text = "Davenport, California",
-                    style = typography.body2
-                )
-                Divider(color = Color.LightGray)
-                Text(
-                    text = "December 2018",
-                    style = typography.body2
-                )
-                Divider(color = Color.Transparent, thickness = 32.dp)
-            }
+            NameList(names, Modifier.weight(1f))
             Counter(
                 count = counterState.value,
                 updateCount = { newCount ->
@@ -86,6 +53,30 @@ class MainActivity : ComponentActivity() {
             )
         }
 
+    }
+
+    @Composable
+    fun NameList(names: List<String>, modifier: Modifier = Modifier) {
+        LazyColumn(modifier = modifier) {
+            items(items = names) { name ->
+                DisplayName(name)
+                Divider(color = Color.Black)
+            }
+        }
+    }
+
+    @Composable
+    fun DisplayName(name: String) {
+        var isSelected by remember { mutableStateOf(false) }
+        val backgroundColor by animateColorAsState(if (isSelected) Color.Red else Color.Transparent)
+
+        Text(
+            text = "Hello $name!",
+            modifier = Modifier
+                .padding(24.dp)
+                .background(color = backgroundColor)
+                .clickable(onClick = { isSelected = !isSelected })
+        )
     }
 
     @Composable
@@ -105,7 +96,7 @@ class MainActivity : ComponentActivity() {
     @Preview(showBackground = true, name = "Text Preview")
     @Composable
     fun DefaultPreview() {
-        NewsStroy()
+        NewNames()
     }
 
 }
