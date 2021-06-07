3d667d1aee54dbfdbaa698a59257773425802d85
Sammy Mutahi <sammymutahi12@gmail.com>
2021-06-07 10:05:26 +0300

    Declaring Composable functions

--- app/src/main/java/com/sammy/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/sammy/jetpackcompose/MainActivity.kt
@@ -13,26 +13,12 @@ import com.sammy.jetpackcompose.ui.theme.JetpackComposeTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            JetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+        setContent { 
+            Greeting(name = "Sammy Mutahi")
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetpackComposeTheme {
-        Greeting("Android")
+    @Composable
+    fun Greeting(name:String){
+        Text(text = "Hello $name!")
     }
 }
