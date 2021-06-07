7ee88a4057a5ebf3adad620b312ba7bb4f1b3204
Sammy Mutahi <sammymutahi12@gmail.com>
2021-06-07 12:42:57 +0300

    Modify the texts using material components Styles

--- app/src/main/java/com/sammy/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/sammy/jetpackcompose/MainActivity.kt
@@ -5,12 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -31,15 +35,29 @@ class MainActivity : ComponentActivity() {
                 contentDescription = null,
                 modifier = Modifier
                     .height(180.dp)
-                    .fillMaxWidth(),
+                    .fillMaxWidth()
+                    .clip(shape = RoundedCornerShape(4.dp)),
                 contentScale = ContentScale.Crop
 
             )
             Spacer(modifier = Modifier.height(16.dp))
 
-            Text(text = "A day in shark fin cove")
-            Text(text = "Davenport, California")
-            Text(text = "December 2018")
+            Text(
+                text = "A day wandering through the sandhills " +
+                        "in Shark Fin Cove, and a few of the " +
+                        "sights I saw",
+                style = typography.h6,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                text = "Davenport, California",
+                style = typography.body2
+            )
+            Text(
+                text = "December 2018",
+                style = typography.body2
+            )
         }
 
     }
