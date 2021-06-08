00c96590497f147936d43f8c34dea82945b121dd
Sammy Mutahi <sammymutahi12@gmail.com>
2021-06-08 14:05:17 +0300

    Add all required dependencies

--- app/src/main/java/com/sammy/jetpackcompose/App.kt
+++ app/src/main/java/com/sammy/jetpackcompose/App.kt
@@ -2,6 +2,7 @@ package com.sammy.jetpackcompose
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import de.hdodenhof.circleimageview.BuildConfig
 import timber.log.Timber
 
 @HiltAndroidApp
